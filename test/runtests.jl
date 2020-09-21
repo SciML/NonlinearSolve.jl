d45be3c77b373f504f6d9b1c0fe230f7878e1c38
Yingbo Ma <mayingbo5@gmail.com>
2020-09-21 11:54:17 -0400

    Test allocation of both problem initialization and solving

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,33 @@
 using NonlinearSolve
 using StaticArrays
-using BenchmarkTools
 using Test
 
-function benchmark_immutable()
-    probN = NonlinearProblem((u,p) -> u .* u .- 2, @SVector[1.0, 1.0])
+function benchmark_immutable(f, u0)
+    probN = NonlinearProblem{false}(f, u0)
     solver = init(probN, NewtonRaphson(), immutable = true, tol = 1e-9)
-    sol = @btime solve!($solver)
-    @test all(sol.u .* sol.u .- 2 .< 1e-9)
+    sol = solve!(solver)
 end
 
-function benchmark_mutable()
-    probN = NonlinearProblem((u,p) -> u .* u .- 2, @SVector[1.0, 1.0])
+function benchmark_mutable(f, u0)
+    probN = NonlinearProblem{false}(f, u0)
     solver = init(probN, NewtonRaphson(), immutable = false, tol = 1e-9)
-    sol = @btime (reinit!($solver, $probN); solve!($solver))
-    @test all(sol.u .* sol.u .- 2 .< 1e-9)
+    sol = (reinit!(solver, probN); solve!(solver))
 end
 
-function benchmark_scalar()
-    probN = NonlinearProblem((u,p) -> u .* u .- 2, 1.0)
-    sol = @btime (solve($probN, ScalarNewton()))
-    @test sol * sol - 2 < 1e-9
+function benchmark_scalar(f, u0)
+    probN = NonlinearProblem{false}(f, u0)
+    sol = (solve(probN, ScalarNewton()))
 end
 
-benchmark_immutable()
-benchmark_mutable()
-benchmark_scalar()
+f, u0 = (u,p) -> u .* u .- 2, @SVector[1.0, 1.0]
+sf, su0 = (u,p) -> u * u - 2, 1.0
+sol = benchmark_immutable(f, u0)
+@test all(sol.u .* sol.u .- 2 .< 1e-9)
+sol = benchmark_mutable(f, u0)
+@test all(sol.u .* sol.u .- 2 .< 1e-9)
+sol = benchmark_scalar(sf, su0)
+@test sol * sol - 2 < 1e-9
+
+@allocated benchmark_immutable(f, u0)
+@allocated benchmark_mutable(f, u0)
+@allocated benchmark_scalar(sf, su0)
