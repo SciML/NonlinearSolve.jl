edae91dc76068a9d0d7efbf5cacb5f14bbb30f07
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-06-23 14:58:16 -0400

    Format SciML Style

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,18 +1,12 @@
 # Put in a separate page so it can be used by SciMLDocs.jl
 
-pages=[
+pages = [
     "Home" => "index.md",
-    "Tutorials" => Any[
-        "tutorials/nonlinear.md",
-        "tutorials/iterator_interface.md"
-    ],
-    "Basics" => Any[
-        "basics/NonlinearProblem.md",
-        "basics/NonlinearFunctions.md",
-        "basics/FAQ.md"
-    ],
-    "Solvers" => Any[
-        "solvers/NonlinearSystemSolvers.md",
-        "solvers/BracketingSolvers.md"
-    ]
+    "Tutorials" => Any["tutorials/nonlinear.md",
+                       "tutorials/iterator_interface.md"],
+    "Basics" => Any["basics/NonlinearProblem.md",
+                    "basics/NonlinearFunctions.md",
+                    "basics/FAQ.md"],
+    "Solvers" => Any["solvers/NonlinearSystemSolvers.md",
+                     "solvers/BracketingSolvers.md"],
 ]
--- src/falsi.jl
+++ src/falsi.jl
@@ -1,45 +1,46 @@
-struct Falsi <: AbstractBracketingAlgorithm
-end
+struct Falsi <: AbstractBracketingAlgorithm end
 
 function alg_cache(alg::Falsi, left, right, p, ::Val{true})
-  nothing
+    nothing
 end
 
 function alg_cache(alg::Falsi, left, right, p, ::Val{false})
-  nothing
+    nothing
 end
 
 function perform_step(solver, alg::Falsi, cache)
-  @unpack f, p, left, right, fl, fr = solver
+    @unpack f, p, left, right, fl, fr = solver
 
-  fzero = zero(fl)
-  fl * fr > fzero && error("Bracket became non-containing in between iterations. This could mean that "
-  * "input function crosses the x axis multiple times. Bisection is not the right method to solve this.")
+    fzero = zero(fl)
+    fl * fr > fzero &&
+        error("Bracket became non-containing in between iterations. This could mean that "
+              *
+              "input function crosses the x axis multiple times. Bisection is not the right method to solve this.")
 
-  mid = (fr * left - fl * right) / (fr - fl)
-  
-  if right == mid || right == mid
-    @set! solver.force_stop = true
-    @set! solver.retcode = FLOATING_POINT_LIMIT
-    return solver
-  end
-  
-  fm = f(mid, p)
+    mid = (fr * left - fl * right) / (fr - fl)
 
-  if iszero(fm)
-    # todo: phase 2 bisection similar to the raw method
-    @set! solver.force_stop = true
-    @set! solver.left = mid
-    @set! solver.fl = fm
-    @set! solver.retcode = EXACT_SOLUTION_LEFT
-  else
-    if sign(fm) == sign(fl)
-      @set! solver.left = mid
-      @set! solver.fl = fm
-    else
-      @set! solver.right = mid
-      @set! solver.fr = fm
+    if right == mid || right == mid
+        @set! solver.force_stop = true
+        @set! solver.retcode = FLOATING_POINT_LIMIT
+        return solver
     end
-  end
-  return solver
+
+    fm = f(mid, p)
+
+    if iszero(fm)
+        # todo: phase 2 bisection similar to the raw method
+        @set! solver.force_stop = true
+        @set! solver.left = mid
+        @set! solver.fl = fm
+        @set! solver.retcode = EXACT_SOLUTION_LEFT
+    else
+        if sign(fm) == sign(fl)
+            @set! solver.left = mid
+            @set! solver.fl = fm
+        else
+            @set! solver.right = mid
+            @set! solver.fr = fm
+        end
+    end
+    return solver
 end
