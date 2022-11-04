0222103d26c1b3367d5aa45979f26577bbea5961
oscarddssmith <oscar.smith@juliacomputing.com>
2022-11-04 17:45:58 -0400

    add SnoopPrecompile and fix _unwrap_val

--- src/NonlinearSolve.jl
+++ src/NonlinearSolve.jl
@@ -18,6 +18,7 @@ abstract type AbstractNewtonAlgorithm{CS, AD, FDT, ST, CJ} <:
               AbstractNonlinearSolveAlgorithm end
 abstract type AbstractImmutableNonlinearSolver <: AbstractNonlinearSolveAlgorithm end
 
+using SciMLBase: _unwrap_val
 include("utils.jl")
 include("jacobian.jl")
 include("types.jl")
@@ -26,6 +27,29 @@ include("bisection.jl")
 include("falsi.jl")
 include("raphson.jl")
 include("scalar.jl")
+
+import SnoopPrecompile
+
+SnoopPrecompile.@precompile_all_calls begin
+    for T in (Float32, Float64)
+        prob_no_brack = NonlinearProblem{false}((u,p) -> u .* u .- p, T(.1), T(2))
+        for alg in (NewtonRaphson,)
+            solve(prob_no_brack, alg(), tol = T(1e-2))
+        end
+        # TODO this is broken?
+        #=for alg in (NewtonRaphson,)
+            for u0 in ([1., 1.], @SVector[1.0, 1.0])
+                u0 = T.(.1)
+                probN = NonlinearProblem{false}((u,p) -> u .* u .- p, u0, T(2))
+                solve(probN, alg(), tol = T(1e-2))
+            end
+        end=#
+        prob_brack = NonlinearProblem{false}((u,p) -> u * u - p, T.((0., 2.)), T(2))
+        for alg in (Bisection, Falsi)
+            solve(prob_brack, alg(), tol = T(1e-2))
+        end
+    end
+end
 
 # DiffEq styled algorithms
 export Bisection, Falsi, NewtonRaphson
