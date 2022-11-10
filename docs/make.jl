6c4a2473f4fe9525af6c68d554186e63b0033c5d
ArnoStrouwen <arno.strouwen@telenet.be>
2022-11-10 17:29:50 +0100

    reproducible docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,8 @@
 using Documenter, NonlinearSolve
 
+cp("./docs/Manifest.toml", "./docs/src/assets/Manifest.toml", force = true)
+cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
+
 include("pages.jl")
 
 makedocs(sitename = "NonlinearSolve.jl",
