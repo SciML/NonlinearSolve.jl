2be1308465b8a07aa649d4e90d657f29056fa2fa
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-12-04 03:10:47 +0100

    format

--- test/gpu.jl
+++ test/gpu.jl
@@ -1,11 +1,11 @@
 using CUDA, NonlinearSolve
 
-A = cu(rand(4,4))
+A = cu(rand(4, 4))
 u0 = cu(rand(4))
 b = cu(rand(4))
 
-function f(du,u,p)
-    du .= A*u .+ b
+function f(du, u, p)
+    du .= A * u .+ b
 end
 
 prob = NonlinearProblem(f, u0)
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,12 @@ const GROUP = get(ENV, "GROUP", "All")
 const is_APPVEYOR = Sys.iswindows() && haskey(ENV, "APPVEYOR")
 
 @time begin
+    if GROUP == "All" || GROUP == "Core"
+        @time @safetestset "Basic Tests + Some AD" begin include("basictests.jl") end
+        @time @safetestset "Sparsity Tests" begin include("sparse.jl") end
+    end
 
-if GROUP == "All" || GROUP == "Core"
-    @time @safetestset "Basic Tests + Some AD" begin include("basictests.jl") end
-    @time @safetestset "Sparsity Tests" begin include("sparse.jl") end
-end
-
-if GROUP == "GPU"
-    @time @safetestset "GPU Tests" begin include("gpu.jl") end
-end
-
+    if GROUP == "GPU"
+        @time @safetestset "GPU Tests" begin include("gpu.jl") end
+    end
 end
