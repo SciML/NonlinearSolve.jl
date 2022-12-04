cdf902b807be3b84ee81293e084dc3dbd1f4013d
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-12-04 03:18:36 +0100

    activate GPU

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,12 @@ const LONGER_TESTS = false
 const GROUP = get(ENV, "GROUP", "All")
 const is_APPVEYOR = Sys.iswindows() && haskey(ENV, "APPVEYOR")
 
+function activate_downstream_env()
+    Pkg.activate("GPU")
+    Pkg.develop(PackageSpec(path = dirname(@__DIR__)))
+    Pkg.instantiate()
+end
+
 @time begin
     if GROUP == "All" || GROUP == "Core"
         @time @safetestset "Basic Tests + Some AD" begin include("basictests.jl") end
@@ -12,6 +18,7 @@ const is_APPVEYOR = Sys.iswindows() && haskey(ENV, "APPVEYOR")
     end
 
     if GROUP == "GPU"
+        activate_downstream_env()
         @time @safetestset "GPU Tests" begin include("gpu.jl") end
     end
 end
