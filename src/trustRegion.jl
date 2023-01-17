18abfba80eda8714e4c2d92e678f4cbe88fb896c
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-01-17 08:21:11 -0500

    Update src/trustRegion.jl

--- src/trustRegion.jl
+++ src/trustRegion.jl
@@ -249,8 +249,8 @@ function perform_step!(cache::TrustRegionCache{false})
 
     if make_new_J
         J = jacobian(cache, f)
-        mul!(cache.H,J,J)
-        mul!(cache.g,J,fu)
+        cache.H = J * J
+        cache.g = J * fu
     end
     dogleg!(cache)
 
