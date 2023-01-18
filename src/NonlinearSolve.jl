1758ecae59d684cf59e96411515595ace2d1972e
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-01-18 04:54:54 -0500

    Update NonlinearSolve.jl

--- src/NonlinearSolve.jl
+++ src/NonlinearSolve.jl
@@ -38,12 +38,12 @@ import SnoopPrecompile
 
 SnoopPrecompile.@precompile_all_calls begin for T in (Float32, Float64)
     prob = NonlinearProblem{false}((u, p) -> u .* u .- p, T(0.1), T(2))
-    for alg in (NewtonRaphson,)
+    for alg in (NewtonRaphson,TrustRegion)
         solve(prob, alg(), abstol = T(1e-2))
     end
 
     prob = NonlinearProblem{true}((du, u, p) -> du[1] = u[1] * u[1] - p[1], T[0.1], T[2])
-    for alg in (NewtonRaphson,)
+    for alg in (NewtonRaphson,TrustRegion)
         solve(prob, alg(), abstol = T(1e-2))
     end
 end end
