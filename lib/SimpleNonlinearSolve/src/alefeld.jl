f2191d60247bd4c6aede40e5a8e8d7c10dbab63c
Huiyu Xie <huiyuxie@Huiyus-MacBook-Pro.local>
2023-03-24 18:20:23 -0700

    newton

--- lib/SimpleNonlinearSolve/src/alefeld.jl
+++ lib/SimpleNonlinearSolve/src/alefeld.jl
@@ -1,29 +1,42 @@
 #struct Alefeld <: AbstractSimpleNonlinearSolveAlgorithm end
 
-# Define subrotine function bracket, check d to see whether the zero is found when using.
+# Define subrotine function bracket, check d to see whether the zero is found.
 function _bracket(f::Function, a, b, c)
-    fc = f(c)
-    if fc == 0
+    if f(c) == 0
         ā, b̄, d = a, b, c
     else
-        fa, fb = f(a), f(b)
-        if fa * fc < 0 
+        if f(a) * f(c) < 0 
             ā, b̄, d = a, c, b
-        elseif fb * fc < 0
+        elseif f(b) * f(c) < 0
             ā, b̄, d = c, b, a
         end
     end
     return ā, b̄, d 
 end
 
-# Define subrotine function 
-#function _newton_quadratic()
-
+# Define subrotine function newton quadratic, return the approximation of unique zero.
+function _newton_quadratic(f::Function, a, b, d, k)
+    A = ((f(b) - f(d)) / (b - d) - (f(a) - f(b)) / (a - b)) / (d - a) 
+    B = (f(b) - f(a)) / (b - a)
+    if A == 0
+        return a - (1 / B) * f(a)
+    elseif A * f(a) > 0
+        rᵢ₋₁ = a 
+    else 
+        rᵢ₋₁ = b
+    end 
+    for i in 1:k
+        rᵢ = rᵢ₋₁ - B * rᵢ₋₁ / (B + A * (2 * rᵢ₋₁ - a - b))
+        rᵢ₋₁ = rᵢ
+    end
+    return rᵢ₋₁
+end
 
 
 # test 
 function fk(x)
-    return 2 * x
+    return x^3
 end
 
-_bracket(fk, -2, 2, 0)
+_newton_quadratic(fk, -2, 4, 100, 2)
+
