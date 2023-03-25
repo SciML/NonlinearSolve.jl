9fad692d07d92ba10d4d35585da1bec12747c6b1
Huiyu Xie <huiyuxie@Huiyus-MacBook-Pro.local>
2023-03-24 20:34:47 -0700

    ipzero

--- lib/SimpleNonlinearSolve/src/alefeld.jl
+++ lib/SimpleNonlinearSolve/src/alefeld.jl
@@ -11,13 +11,15 @@ function _bracket(f::Function, a, b, c)
             ā, b̄, d = c, b, a
         end
     end
+
     return ā, b̄, d 
 end
 
-# Define subrotine function newton quadratic, return the approximation of unique zero.
+# Define subrotine function newton quadratic, return the approximation of zero.
 function _newton_quadratic(f::Function, a, b, d, k)
     A = ((f(b) - f(d)) / (b - d) - (f(a) - f(b)) / (a - b)) / (d - a) 
     B = (f(b) - f(a)) / (b - a)
+
     if A == 0
         return a - (1 / B) * f(a)
     elseif A * f(a) > 0
@@ -25,13 +27,29 @@ function _newton_quadratic(f::Function, a, b, d, k)
     else 
         rᵢ₋₁ = b
     end 
+
     for i in 1:k
         rᵢ = rᵢ₋₁ - B * rᵢ₋₁ / (B + A * (2 * rᵢ₋₁ - a - b))
         rᵢ₋₁ = rᵢ
     end
+
     return rᵢ₋₁
 end
 
+# Define subrotine function ipzero, also return the approximation of zero.
+function _ipzero(f::Function, a, b, c, d)
+    Q₁₁ = (c - d) * f(c) / (f(d) - f(c))
+    Q₂₁ = (b - c) * f(b) / (f(c) - f(b))
+    Q₃₁ = (a - b) * f(a) / (f(b) - f(a))
+    D₂₁ = (b - c) * f(c) / (f(c) - f(b))
+    D₃₁ = (a - b) * f(b) / (f(b) - f(a))
+    Q₂₂ = (D₂₁ - Q₁₁) * f(b) / (f(d) - f(b))
+    Q₃₂ = (D₃₁ - Q₂₁) * f(a) / (f(c) - f(a))
+    D₃₂ = (D₃₁ - Q₂₁) * f(c) / (f(c) - f(a))
+    Q₃₃ = (D₃₂ - Q₂₂) * f(a) / (f(d) - f(a))
+
+    return a + Q₃₁ + Q₃₂ + Q₃₃
+end
 
 # test 
 function fk(x)
