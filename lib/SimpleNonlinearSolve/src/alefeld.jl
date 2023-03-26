d2c82b214de0230225b26e125efd66d02a53527d
Huiyu Xie <huiyuxie@Huiyus-MacBook-Pro.local>
2023-03-25 23:55:18 -0700

    add solve

--- lib/SimpleNonlinearSolve/src/alefeld.jl
+++ lib/SimpleNonlinearSolve/src/alefeld.jl
@@ -1,8 +1,111 @@
-#struct Alefeld <: AbstractSimpleNonlinearSolveAlgorithm end
+struct Alefeld <: AbstractBracketingAlgorithm end
 
-# Define subrotine function bracket, check d to see whether the zero is found.
+function SciMLBase.__solve(prob::NonlinearProblem,
+                            alg::Alefeld, args...; abstol = nothing,
+                            reltol = nothing,
+                            maxiters = 1000, kwargs...)
+                            
+    f = Base.Fix2(prob.f, prob.p)
+    a, b = prob.tspan
+    c = a - (b - a) / (f(b) - f(a)) * f(a)
+    
+    fc = f(c)
+    if iszero(fc)
+        return SciMLBase.build_solution(prob, alg, c, fc;
+                                        retcode = ReturnCode.Success, 
+                                        left = a,
+                                        right = b)
+    end
+    a, b, d = _bracket(f, a, b, c)
+    e = 0   # Set e as 0 before interation to avoid a non-value f(e)
+
+    for i in 2:maxiters
+        # The first bracketing block
+        f₁, f₂, f₃, f₄ = f(a), f(b), f(d), f(e)
+        if i == 2 || (f₁ == f₂ || f₁ == f₃ || f₁ == f₄ || f₂ == f₃ || f₂ == f₄ || f₃ == f₄)
+            c = _newton_quadratic(f, a, b, d, 2)
+        else 
+            c = _ipzero(f, a, b, d, e)
+            if (c - a) * (c - b) ≥ 0
+                c = _newton_quadratic(f, a, b, d, 2)
+            end
+        end 
+        ē, fc = d, f(c)   
+        iszero(fc) &&
+            return SciMLBase.build_solution(prob, alg, c, fc;
+                                        retcode = ReturnCode.Success, 
+                                        left = a,
+                                        right = b)
+        ā, b̄, d̄ = _bracket(f, a, b, c) 
+
+        # The second bracketing block
+        f₁, f₂, f₃, f₄ = f(ā), f(b̄), f(d̄), f(ē)
+        if f₁ == f₂ || f₁ == f₃ || f₁ == f₄ || f₂ == f₃ || f₂ == f₄ || f₃ == f₄
+            c = _newton_quadratic(f, ā, b̄, d̄, 3)
+        else 
+            c = _ipzero(f, ā, b̄, d̄, ē)
+            if (c - ā) * (c - b̄) ≥ 0
+                c = _newton_quadratic(f, ā, b̄, d̄, 3)
+            end
+        end
+        fc = f(c)
+        iszero(fc) &&
+            return SciMLBase.build_solution(prob, alg, c, fc;
+                                        retcode = ReturnCode.Success, 
+                                        left = a,
+                                        right = b)
+        ā, b̄, d̄ = _bracket(f, ā, b̄, c) 
+
+        # The third bracketing block
+        if abs(f(ā)) < abs(f(b̄))
+            u = ā
+        else
+            u = b̄
+        end
+        c = u - 2 * (b̄ - ā) / (f(b̄) - f(ā)) * f(u)
+        if (abs(c - u)) > 0.5 * (b̄ - ā)
+            c = 0.5 * (ā + b̄)
+        end
+        fc = f(c)
+        iszero(fc) &&
+            return SciMLBase.build_solution(prob, alg, c, fc;
+                                        retcode = ReturnCode.Success, 
+                                        left = a,
+                                        right = b)
+        ā, b̄, d = _bracket(f, ā, b̄, c) 
+
+        # The last bracketing block
+        if b̄ - ā < 0.5 * (b - a)
+            a, b, e = ā, b̄, d̄
+        else
+            e = d
+            c = 0.5 * (ā + b̄)
+            fc = f(c)
+            iszero(fc) &&
+            return SciMLBase.build_solution(prob, alg, c, fc;
+                                        retcode = ReturnCode.Success, 
+                                        left = a,
+                                        right = b)
+            a, b, d = _bracket(f, ā, b̄, c)
+        end
+    end
+
+    # Reassign the value a, b, and c
+    if b == c
+        b = d
+    elseif a == c
+        a = d
+    end
+    fc = f(c)
+
+    # Reuturn solution when run out of max interation
+    return SciMLBase.build_solution(prob, alg, c, fc; retcode = ReturnCode.MaxIters,
+                                    left = a, right = b)
+end
+
+# Define subrotine function bracket, check fc before bracket to return solution
 function _bracket(f::Function, a, b, c)
-    if f(c) == 0
+    if iszero(f(c))
         ā, b̄, d = a, b, c
     else
         if f(a) * f(c) < 0 
@@ -15,12 +118,12 @@ function _bracket(f::Function, a, b, c)
     return ā, b̄, d 
 end
 
-# Define subrotine function newton quadratic, return the approximation of zero.
+# Define subrotine function newton quadratic, return the approximation of zero
 function _newton_quadratic(f::Function, a, b, d, k)
     A = ((f(b) - f(d)) / (b - d) - (f(a) - f(b)) / (a - b)) / (d - a) 
     B = (f(b) - f(a)) / (b - a)
 
-    if A == 0
+    if iszero(A)
         return a - (1 / B) * f(a)
     elseif A * f(a) > 0
         rᵢ₋₁ = a 
@@ -36,7 +139,7 @@ function _newton_quadratic(f::Function, a, b, d, k)
     return rᵢ₋₁
 end
 
-# Define subrotine function ipzero, also return the approximation of zero.
+# Define subrotine function ipzero, also return the approximation of zero
 function _ipzero(f::Function, a, b, c, d)
     Q₁₁ = (c - d) * f(c) / (f(d) - f(c))
     Q₂₁ = (b - c) * f(b) / (f(c) - f(b))
@@ -50,11 +153,3 @@ function _ipzero(f::Function, a, b, c, d)
 
     return a + Q₃₁ + Q₃₂ + Q₃₃
 end
-
-# test 
-function fk(x)
-    return x^3
-end
-
-_newton_quadratic(fk, -2, 4, 100, 2)
-
