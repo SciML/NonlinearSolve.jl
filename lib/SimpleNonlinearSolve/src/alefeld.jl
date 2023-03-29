0bb2ee41586dd35bf48f642511ff6b1bd334befa
Huiyu Xie <huiyuxie.sde@gmail.com>
2023-03-29 09:21:22 -0700

    Update src/alefeld.jl
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- lib/SimpleNonlinearSolve/src/alefeld.jl
+++ lib/SimpleNonlinearSolve/src/alefeld.jl
@@ -6,7 +6,6 @@ An implementation of algorithm 4.2 from [Alefeld](https://dl.acm.org/doi/10.1145
 The paper brought up two new algorithms. Here choose to implement algorithm 4.2 rather than 
 algorithm 4.1 because, in certain sense, the second algorithm(4.2) is an optimal procedure.
 """
-
 struct Alefeld <: AbstractBracketingAlgorithm end
 
 function SciMLBase.solve(prob::NonlinearProblem,
