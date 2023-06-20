15234b40d74e6955b12f7c0e54fb9aa34987146d
Yash Raj Singh <yash.raj270998@gmail.com>
2023-06-20 13:32:58 +0200

    updated runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,6 +15,7 @@ end
     if GROUP == "All" || GROUP == "Core"
         @time @safetestset "Basic Tests + Some AD" include("basictests.jl")
         @time @safetestset "Sparsity Tests" include("sparse.jl")
+        @time @safetestset "Convergence Tests" include("convergencetests.jl")
     end
 
     if GROUP == "GPU"
