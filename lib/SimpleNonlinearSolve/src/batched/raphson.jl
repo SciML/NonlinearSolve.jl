52b5bacf974cd41ed37ef292ba7edd4431d65411
Avik Pal <avikpal@mit.edu>
2023-06-27 17:01:35 -0400

    Remove docstrings to ensure users don't directly construct the batched solvers

--- lib/SimpleNonlinearSolve/src/batched/raphson.jl
+++ lib/SimpleNonlinearSolve/src/batched/raphson.jl
@@ -1,19 +1,3 @@
-"""
-    SimpleBatchedNewtonRaphson(; chunk_size = Val{0}(),
-        autodiff = Val{true}(),
-        diff_type = Val{:forward},
-        termination_condition = NLSolveTerminationCondition(NLSolveTerminationMode.NLSolveDefault;
-            abstol = nothing,
-            reltol = nothing))
-
-A low-overhead batched implementation of Newton-Raphson capable of solving multiple
-nonlinear problems simultaneously.
-
-!!! note
-
-    To use the `batched` version, remember to load `AbstractDifferentiation` and
-    `LinearSolve`.
-"""
 struct SimpleBatchedNewtonRaphson{AD, LS, TC <: NLSolveTerminationCondition} <:
     AbstractBatchedNonlinearSolveAlgorithm
     autodiff::AD
