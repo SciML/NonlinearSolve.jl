504ab19c750efa76093d3ef00a41d843777667a7
Yash Raj Singh <yash.raj270998@gmail.com>
2023-07-07 02:26:37 +0200

    final hyper params, tol and some modifications

--- lib/SimpleNonlinearSolve/src/itp.jl
+++ lib/SimpleNonlinearSolve/src/itp.jl
@@ -11,7 +11,7 @@ struct Itp <: AbstractBracketingAlgorithm
     k1::Real
     k2::Real
     n0::Int
-    function Itp(;k1::Real = 0.3, k2::Real = 2.0, n0::Int = 1)
+    function Itp(;k1::Real = 0.007, k2::Real = 1.5, n0::Int = 10)
         if k1 < 0
             error("Hyper-parameter κ₁ should not be negative")
         end
@@ -26,7 +26,7 @@ struct Itp <: AbstractBracketingAlgorithm
 end
 
 function SciMLBase.solve(prob::IntervalNonlinearProblem, alg::Itp,
-                            args...; abstol = 1.0e-8,
+                            args...; abstol = 1.0e-15,
                             maxiters = 1000, kwargs...)
     f = Base.Fix2(prob.f, prob.p)
     left, right = prob.tspan # a and b
@@ -46,7 +46,6 @@ function SciMLBase.solve(prob::IntervalNonlinearProblem, alg::Itp,
     k2 = alg.k2
     n0 = alg.n0
     n_h = ceil(log2((right - left) / (2 * ϵ)))
-    n_max = n_h + n0
     mid = (left + right) / 2
     x_f = (fr * left - fl * right) / (fr - fl)
     xt = left
@@ -54,11 +53,12 @@ function SciMLBase.solve(prob::IntervalNonlinearProblem, alg::Itp,
     r = zero(left) #minmax radius
     δ = zero(left) # truncation error
     σ = 1.0
+    ϵ_s = ϵ * 2^(n_h + n0)
     i = 0 #iteration
     while i <= maxiters
         #mid = (left + right) / 2
-        r = ϵ * 2 ^ (n_max - i) - ((right - left) / 2)
-        δ = k1 * (right - left) ^ k2
+        r = ϵ_s - ((right - left) / 2)
+        δ = k1 * ((right - left) ^ k2)
 
         ## Interpolation step ##
         x_f =  (fr * left - fl * right) / (fr - fl)
@@ -92,6 +92,7 @@ function SciMLBase.solve(prob::IntervalNonlinearProblem, alg::Itp,
         end
         i += 1
         mid = (left + right) / 2
+        ϵ_s /= 2
 
         if (right - left < 2 * ϵ)
             return SciMLBase.build_solution(prob, alg, mid, f(mid);
