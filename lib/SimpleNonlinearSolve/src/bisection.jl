5b0ec527e2c74dd778ba31924556a4905fb18780
Yash Raj Singh <yash.raj270998@gmail.com>
2023-09-01 02:03:41 +0200

    tolerance fix bisection

--- lib/SimpleNonlinearSolve/src/bisection.jl
+++ lib/SimpleNonlinearSolve/src/bisection.jl
@@ -20,12 +20,12 @@ function Bisection(; exact_left = false, exact_right = false)
 end
 
 function SciMLBase.solve(prob::IntervalNonlinearProblem, alg::Bisection, args...;
-    maxiters = 1000,
+    maxiters = 1000, abstol = nothing,
     kwargs...)
     f = Base.Fix2(prob.f, prob.p)
     left, right = prob.tspan
     fl, fr = f(left), f(right)
-
+    atol = abstol !== nothing ? abstol : eps(1.0)^(3 // 5)
     if iszero(fl)
         return SciMLBase.build_solution(prob, alg, left, fl;
             retcode = ReturnCode.ExactSolutionLeft, left = left,
