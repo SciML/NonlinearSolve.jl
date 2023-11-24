b50ece4301342fe9145301d00974d299f38cdc9d
Avik Pal <avikpal@mit.edu>
2023-11-23 19:18:29 -0500

    Move things around a bit

--- lib/SimpleNonlinearSolve/src/SimpleNonlinearSolve.jl
+++ lib/SimpleNonlinearSolve/src/SimpleNonlinearSolve.jl
@@ -28,57 +28,57 @@ include("utils.jl")
 include("rewrite_inplace.jl")
 
 # Nonlinear Solvera
-include("raphson.jl")
-include("broyden.jl")
-# include("lbroyden.jl")
-include("klement.jl")
-# include("trustRegion.jl")
-# include("halley.jl")
-# include("dfsane.jl")
+include("nlsolve/raphson.jl")
+include("nlsolve/broyden.jl")
+# include("nlsolve/lbroyden.jl")
+include("nlsolve/klement.jl")
+# include("nlsolve/trustRegion.jl")
+# include("nlsolve/halley.jl")
+# include("nlsolve/dfsane.jl")
 
 # Interval Nonlinear Solvers
-include("bisection.jl")
-include("falsi.jl")
-include("ridder.jl")
-include("brent.jl")
-include("alefeld.jl")
-include("itp.jl")
+include("bracketing/bisection.jl")
+include("bracketing/falsi.jl")
+include("bracketing/ridder.jl")
+include("bracketing/brent.jl")
+include("bracketing/alefeld.jl")
+include("bracketing/itp.jl")
 
 # AD
 # include("ad.jl")
 
-# ## Default algorithm
+## Default algorithm
 
-# # Set the default bracketing method to ITP
+# Set the default bracketing method to ITP
 
-# function SciMLBase.solve(prob::IntervalNonlinearProblem; kwargs...)
-#     SciMLBase.solve(prob, ITP(); kwargs...)
-# end
+function SciMLBase.solve(prob::IntervalNonlinearProblem; kwargs...)
+    return solve(prob, ITP(); kwargs...)
+end
 
-# function SciMLBase.solve(prob::IntervalNonlinearProblem, alg::Nothing,
-#     args...; kwargs...)
-#     SciMLBase.solve(prob, ITP(), args...; kwargs...)
-# end
+function SciMLBase.solve(prob::IntervalNonlinearProblem, alg::Nothing,
+        args...; kwargs...)
+    return solve(prob, ITP(), args...; kwargs...)
+end
 
 # import PrecompileTools
 
 @setup_workload begin
     for T in (Float32, Float64)
         # prob_no_brack = NonlinearProblem{false}((u, p) -> u .* u .- p, T(0.1), T(2))
-#         for alg in (SimpleNewtonRaphson, SimpleHalley, Broyden, Klement, SimpleTrustRegion,
-#             SimpleDFSane)
-#             solve(prob_no_brack, alg(), abstol = T(1e-2))
-#         end
+        #         for alg in (SimpleNewtonRaphson, SimpleHalley, Broyden, Klement, SimpleTrustRegion,
+        #             SimpleDFSane)
+        #             solve(prob_no_brack, alg(), abstol = T(1e-2))
+        #         end
 
-#         #=
-#         for alg in (SimpleNewtonRaphson,)
-#             for u0 in ([1., 1.], StaticArraysCore.SA[1.0, 1.0])
-#                 u0 = T.(.1)
-#                 probN = NonlinearProblem{false}((u,p) -> u .* u .- p, u0, T(2))
-#                 solve(probN, alg(), tol = T(1e-2))
-#             end
-#         end
-#         =#
+        #         #=
+        #         for alg in (SimpleNewtonRaphson,)
+        #             for u0 in ([1., 1.], StaticArraysCore.SA[1.0, 1.0])
+        #                 u0 = T.(.1)
+        #                 probN = NonlinearProblem{false}((u,p) -> u .* u .- p, u0, T(2))
+        #                 solve(probN, alg(), tol = T(1e-2))
+        #             end
+        #         end
+        #         =#
 
         prob_brack = IntervalNonlinearProblem{false}((u, p) -> u * u - p,
             T.((0.0, 2.0)), T(2))
