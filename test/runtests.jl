8e8dad891dca6b2f844368b0fb3bea6530951fb0
Arno Strouwen <arno.strouwen@telenet.be>
2023-11-26 05:23:24 +0100

    add Aqua QA

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,6 +13,7 @@ end
 
 @time begin
     if GROUP == "All" || GROUP == "Core"
+        @time @safetestset "Quality Assurance" include("qa.jl")
         @time @safetestset "Basic Tests + Some AD" include("basictests.jl")
         @time @safetestset "Sparsity Tests" include("sparse.jl")
         @time @safetestset "Polyalgs" include("polyalgs.jl")
