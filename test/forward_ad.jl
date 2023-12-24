e4534ed768e385aac1fb6a74529158a3e020bd81
Avik Pal <avikpal@iitk.ac.in>
2023-12-24 01:50:46 -0500

    Add inplace and cached ForwardDiff rules

--- test/forward_ad.jl
+++ test/forward_ad.jl
@@ -8,18 +8,33 @@ jacobian_f(::Number, p::Number) = 1 / (2 * √p)
 jacobian_f(u, p::Number) = one.(u) .* (1 / (2 * √p))
 jacobian_f(u, p::AbstractArray) = diagm(vec(@. 1 / (2 * √p)))
 
-function solve_with(::Val{iip}, u, alg) where {iip}
-    f = if iip
+function solve_with(::Val{mode}, u, alg) where {mode}
+    f = if mode === :iip
         solve_iip(p) = solve(NonlinearProblem(test_f!, u, p), alg).u
-    else
+    elseif mode === :iip_cache
+        function solve_iip_init(p)
+            cache = SciMLBase.init(NonlinearProblem(test_f!, u, p), alg)
+            return SciMLBase.solve!(cache).u
+        end
+    elseif mode === :oop
         solve_oop(p) = solve(NonlinearProblem(test_f, u, p), alg).u
+    elseif mode === :oop_cache
+        function solve_oop_init(p)
+            cache = SciMLBase.init(NonlinearProblem(test_f, u, p), alg)
+            return SciMLBase.solve!(cache).u
+        end
     end
     return f
 end
 
-__can_inplace(::Number) = false
-__can_inplace(::AbstractArray) = true
-__can_inplace(::StaticArray) = false
+__compatible(::Any, ::Val{:oop}) = true
+__compatible(::Any, ::Val{:oop_cache}) = true
+__compatible(::Number, ::Val{:iip}) = false
+__compatible(::AbstractArray, ::Val{:iip}) = true
+__compatible(::StaticArray, ::Val{:iip}) = false
+__compatible(::Number, ::Val{:iip_cache}) = false
+__compatible(::AbstractArray, ::Val{:iip_cache}) = true
+__compatible(::StaticArray, ::Val{:iip_cache}) = false
 
 __compatible(::Any, ::Number) = true
 __compatible(::Number, ::AbstractArray) = false
@@ -32,37 +47,49 @@ __compatible(u::StaticArray, ::SciMLBase.AbstractNonlinearAlgorithm) = true
 __compatible(u::StaticArray, ::Union{CMINPACK, NLsolveJL}) = false
 __compatible(u, ::Nothing) = true
 
+__compatible(::Any, ::Any) = true
+__compatible(::CMINPACK, ::Val{:iip_cache}) = false
+__compatible(::CMINPACK, ::Val{:oop_cache}) = false
+__compatible(::NLsolveJL, ::Val{:iip_cache}) = false
+__compatible(::NLsolveJL, ::Val{:oop_cache}) = false
+
 @testset "ForwardDiff.jl Integration: $(alg)" for alg in (NewtonRaphson(), TrustRegion(),
     LevenbergMarquardt(), PseudoTransient(; alpha_initial = 10.0), Broyden(), Klement(),
     DFSane(), nothing, NLsolveJL(), CMINPACK())
     us = (2.0, @SVector[1.0, 1.0], [1.0, 1.0], ones(2, 2), @SArray ones(2, 2))
 
     @testset "Scalar AD" begin
-        for p in 1.0:0.1:100.0
-            for u0 in us
-                __compatible(u0, alg) || continue
-                sol = solve(NonlinearProblem(test_f, u0, p), alg)
-                if SciMLBase.successful_retcode(sol)
-                    gs = abs.(ForwardDiff.derivative(solve_with(Val{false}(), u0, alg), p))
-                    gs_true = abs.(jacobian_f(u0, p))
-                    if !(isapprox(gs, gs_true, atol = 1e-5))
-                        @show sol.retcode, sol.u
-                        @error "ForwardDiff Failed for u0=$(u0) and p=$(p) with $(alg)" forwardiff_gradient=gs true_gradient=gs_true
-                    else
-                        @test abs.(gs)≈abs.(gs_true) atol=1e-5
-                    end
+        for p in 1.0:0.1:100.0, u0 in us, mode in (:iip, :oop, :iip_cache, :oop_cache)
+            __compatible(u0, alg) || continue
+            __compatible(u0, Val(mode)) || continue
+            __compatible(alg, Val(mode)) || continue
+
+            sol = solve(NonlinearProblem(test_f, u0, p), alg)
+            if SciMLBase.successful_retcode(sol)
+                gs = abs.(ForwardDiff.derivative(solve_with(Val{mode}(), u0, alg), p))
+                gs_true = abs.(jacobian_f(u0, p))
+                if !(isapprox(gs, gs_true, atol = 1e-5))
+                    @show sol.retcode, sol.u
+                    @error "ForwardDiff Failed for u0=$(u0) and p=$(p) with $(alg)" forwardiff_gradient=gs true_gradient=gs_true
+                else
+                    @test abs.(gs)≈abs.(gs_true) atol=1e-5
                 end
             end
         end
     end
 
     @testset "Jacobian" begin
-        for u0 in us, p in ([2.0, 1.0], [2.0 1.0; 3.0 4.0])
+        for u0 in us, p in ([2.0, 1.0], [2.0 1.0; 3.0 4.0]),
+            mode in (:iip, :oop, :iip_cache, :oop_cache)
+
             __compatible(u0, p) || continue
             __compatible(u0, alg) || continue
+            __compatible(u0, Val(mode)) || continue
+            __compatible(alg, Val(mode)) || continue
+
             sol = solve(NonlinearProblem(test_f, u0, p), alg)
             if SciMLBase.successful_retcode(sol)
-                gs = abs.(ForwardDiff.jacobian(solve_with(Val{false}(), u0, alg), p))
+                gs = abs.(ForwardDiff.jacobian(solve_with(Val{mode}(), u0, alg), p))
                 gs_true = abs.(jacobian_f(u0, p))
                 if !(isapprox(gs, gs_true, atol = 1e-5))
                     @show sol.retcode, sol.u
