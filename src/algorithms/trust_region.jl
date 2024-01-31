f68678a88f40b8a96a477a01777fd87ed3399b0c
oscarddssmith <oscar.smith@juliacomputing.com>
2024-01-30 19:35:20 -0500

    fix autodiff=AutoFiniteDiff() for TrustRegion

--- src/algorithms/trust_region.jl
+++ src/algorithms/trust_region.jl
@@ -24,9 +24,13 @@ function TrustRegion(; concrete_jac = nothing, linsolve = nothing, precs = DEFAU
         initial_trust_radius::Real = 0 // 1, step_threshold::Real = 1 // 10000,
         shrink_threshold::Real = 1 // 4, expand_threshold::Real = 3 // 4,
         shrink_factor::Real = 1 // 4, expand_factor::Real = 2 // 1,
-        max_shrink_times::Int = 32, vjp_autodiff = nothing, autodiff = nothing)
+        max_shrink_times::Int = 32, autodiff = nothing, vjp_autodiff = autodiff)
     descent = Dogleg(; linsolve, precs)
-    forward_ad = autodiff isa ADTypes.AbstractForwardMode ? autodiff : nothing
+    if autodiff isa Union{ADTypes.AbstractForwardMode, ADTypes.AbstractFiniteDifferencesMode}
+        forward_ad = autodiff
+    else
+        forward_ad = nothing
+    end
     trustregion = GenericTrustRegionScheme(; method = radius_update_scheme, step_threshold,
         shrink_threshold, expand_threshold, shrink_factor, expand_factor,
         reverse_ad = vjp_autodiff, forward_ad)
