b0f697f5f990ca5fc410dcddf1eca322470d60a6
Avik Pal <avikpal@mit.edu>
2024-09-16 20:30:27 -0400

    feat: ForwardDiff support in NonlinearSolveBase

--- lib/NonlinearSolveBase/ext/NonlinearSolveBaseForwardDiffExt.jl
+++ lib/NonlinearSolveBase/ext/NonlinearSolveBaseForwardDiffExt.jl
@@ -1,9 +1,82 @@
 module NonlinearSolveBaseForwardDiffExt
 
+using CommonSolve: solve
+using FastClosures: @closure
 using ForwardDiff: ForwardDiff, Dual
-using NonlinearSolveBase: Utils
+using SciMLBase: SciMLBase, IntervalNonlinearProblem, NonlinearProblem,
+                 NonlinearLeastSquaresProblem, remake
+
+using NonlinearSolveBase: NonlinearSolveBase, ImmutableNonlinearProblem, Utils
 
 Utils.value(::Type{Dual{T, V, N}}) where {T, V, N} = V
 Utils.value(x::Dual) = Utils.value(ForwardDiff.value(x))
+
+function NonlinearSolveBase.nonlinearsolve_forwarddiff_solve(
+        prob::Union{IntervalNonlinearProblem, NonlinearProblem, ImmutableNonlinearProblem},
+        alg, args...; kwargs...)
+    p = Utils.value(prob.p)
+    if prob isa IntervalNonlinearProblem
+        tspan = Utils.value.(prob.tspan)
+        newprob = IntervalNonlinearProblem(prob.f, tspan, p; prob.kwargs...)
+    else
+        newprob = remake(prob; p, u0 = Utils.value(prob.u0))
+    end
+
+    sol = solve(newprob, alg, args...; kwargs...)
+
+    uu = sol.u
+    Jₚ = nonlinearsolve_∂f_∂p(prob, prob.f, uu, p)
+    Jᵤ = nonlinearsolve_∂f_∂u(prob, prob.f, uu, p)
+    z = -Jᵤ \ Jₚ
+    pp = prob.p
+    sumfun = ((z, p),) -> map(Base.Fix2(*, ForwardDiff.partials(p)), z)
+
+    if uu isa Number
+        partials = sum(sumfun, zip(z, pp))
+    elseif p isa Number
+        partials = sumfun((z, pp))
+    else
+        partials = sum(sumfun, zip(eachcol(z), pp))
+    end
+
+    return sol, partials
+end
+
+function nonlinearsolve_∂f_∂p(prob, f::F, u, p) where {F}
+    if isinplace(prob)
+        f = @closure p -> begin
+            du = Utils.safe_similar(u, promote_type(eltype(u), eltype(p)))
+            f(du, u, p)
+            return du
+        end
+    else
+        f = Base.Fix1(f, u)
+    end
+    if p isa Number
+        return Utils.safe_reshape(ForwardDiff.derivative(f, p), :, 1)
+    elseif u isa Number
+        return Utils.safe_reshape(ForwardDiff.gradient(f, p), 1, :)
+    else
+        return ForwardDiff.jacobian(f, p)
+    end
+end
+
+function nonlinearsolve_∂f_∂u(prob, f::F, u, p) where {F}
+    if isinplace(prob)
+        return ForwardDiff.jacobian(
+            @closure((du, u)->f(du, u, p)), Utils.safe_similar(u), u)
+    end
+    return ForwardDiff.jacobian(Base.Fix2(f, p), u)
+end
+
+function NonlinearSolveBase.nonlinearsolve_dual_solution(u::Number, partials,
+        ::Union{<:AbstractArray{<:Dual{T, V, P}}, Dual{T, V, P}}) where {T, V, P}
+    return Dual{T, V, P}(u, partials)
+end
+
+function NonlinearSolveBase.nonlinearsolve_dual_solution(u::AbstractArray, partials,
+        ::Union{<:AbstractArray{<:Dual{T, V, P}}, Dual{T, V, P}}) where {T, V, P}
+    return map(((uᵢ, pᵢ),) -> Dual{T, V, P}(uᵢ, pᵢ), zip(u, Utils.restructure(u, partials)))
+end
 
 end
