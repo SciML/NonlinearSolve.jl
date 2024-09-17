b4d5e1d735eef287acf57326139e5dc2945617e1
Avik Pal <avikpal@mit.edu>
2024-09-17 12:56:21 -0400

    fix: extension for forward AD support

--- lib/BracketingNonlinearSolve/test/rootfind_tests.jl
+++ lib/BracketingNonlinearSolve/test/rootfind_tests.jl
@@ -1,6 +1,4 @@
 @testsnippet RootfindingTestSnippet begin
-    using NonlinearSolveBase, BracketingNonlinearSolve
-
     quadratic_f(u, p) = u .* u .- p
     quadratic_f!(du, u, p) = (du .= u .* u .- p)
     quadratic_f2(u, p) = @. p[1] * u * u - p[2]
