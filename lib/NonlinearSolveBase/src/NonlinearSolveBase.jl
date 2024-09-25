2d0768ad2c35c026467b34f738892221e5e67cca
Avik Pal <avikpal@mit.edu>
2024-09-25 18:04:59 -0400

    feat: automatic backend selection for autodiff

--- lib/NonlinearSolveBase/src/NonlinearSolveBase.jl
+++ lib/NonlinearSolveBase/src/NonlinearSolveBase.jl
@@ -1,8 +1,11 @@
 module NonlinearSolveBase
 
+using ADTypes: ADTypes, AbstractADType, ForwardMode, ReverseMode
 using ArrayInterface: ArrayInterface
 using Compat: @compat
 using ConcreteStructs: @concrete
+using DifferentiationInterface: DifferentiationInterface
+using EnzymeCore: EnzymeCore
 using FastClosures: @closure
 using LinearAlgebra: norm
 using Markdown: @doc_str
@@ -13,6 +16,8 @@ using SciMLBase: SciMLBase, ReturnCode, AbstractODEIntegrator, AbstractNonlinear
                  isinplace, warn_paramtype
 using StaticArraysCore: StaticArray
 
+const DI = DifferentiationInterface
+
 include("public.jl")
 include("utils.jl")
 
@@ -20,9 +25,13 @@ include("immutable_problem.jl")
 include("common_defaults.jl")
 include("termination_conditions.jl")
 
+include("autodiff.jl")
+
 # Unexported Public API
 @compat(public, (L2_NORM, Linf_NORM, NAN_CHECK, UNITLESS_ABS2, get_tolerance))
 @compat(public, (nonlinearsolve_forwarddiff_solve, nonlinearsolve_dual_solution))
+@compat(public, (select_forward_mode_autodiff, select_reverse_mode_autodiff,
+    select_jacobian_autodiff))
 
 export RelTerminationMode, AbsTerminationMode, NormTerminationMode, RelNormTerminationMode,
        AbsNormTerminationMode, RelNormSafeTerminationMode, AbsNormSafeTerminationMode,
