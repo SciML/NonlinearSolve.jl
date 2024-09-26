a25aec4614826e1a55352cb0725ad4f7c8cb0549
Avik Pal <avikpal@mit.edu>
2024-09-26 05:20:29 -0400

    fix: forward reinit_cache to SciMLBase.reinit

--- src/globalization/line_search.jl
+++ src/globalization/line_search.jl
@@ -27,3 +27,7 @@ Base.@deprecate LiFukushimaLineSearch(; nan_max_iter::Int = 5, kwargs...) LineSe
 function callback_into_cache!(topcache, cache::AbstractLineSearchCache, args...)
     LineSearch.callback_into_cache!(cache, get_fu(topcache))
 end
+
+function reinit_cache!(cache::AbstractLineSearchCache, args...; kwargs...)
+    return SciMLBase.reinit!(cache, args...; kwargs...)
+end
