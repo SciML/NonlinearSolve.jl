51b24f9760c987385db8f597981fb2219a55f919
Avik Pal <avikpal@mit.edu>
2024-10-06 21:35:39 -0400

    chore: run formatter

--- lib/SimpleNonlinearSolve/test/runtests.jl
+++ lib/SimpleNonlinearSolve/test/runtests.jl
@@ -7,5 +7,5 @@ const GROUP = lowercase(get(ENV, "GROUP", "All"))
 if GROUP == "all"
     @run_package_tests
 else
-    @run_package_tests filter=ti->(Symbol(GROUP) in ti.tags)
+    @run_package_tests filter = ti -> (Symbol(GROUP) in ti.tags)
 end
