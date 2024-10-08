e8bdc15758fc9cae6c8c0660046a49e5d4bea1d8
Avik Pal <avikpal@mit.edu>
2024-10-07 20:26:47 -0400

    fix: QA for NonlinearSolveBase

--- lib/NonlinearSolveBase/test/runtests.jl
+++ lib/NonlinearSolveBase/test/runtests.jl
@@ -1,1 +1,24 @@
+using InteractiveUtils, Test
 
+@info sprint(InteractiveUtils.versioninfo)
+
+# Changing any code here triggers all the other tests to be run. So we intentionally
+# keep the tests here minimal.
+@testset "NonlinearSolveBase.jl" begin
+    @testset "Aqua" begin
+        using Aqua, NonlinearSolveBase
+
+        Aqua.test_all(NonlinearSolveBase; piracies = false, ambiguities = false)
+        Aqua.test_piracies(NonlinearSolveBase)
+        Aqua.test_ambiguities(NonlinearSolveBase; recursive = false)
+    end
+
+    @testset "Explicit Imports" begin
+        import ForwardDiff, SparseArrays, DiffEqBase
+        using ExplicitImports, NonlinearSolveBase
+
+        @test check_no_implicit_imports(NonlinearSolveBase; skip = (Base, Core)) === nothing
+        @test check_no_stale_explicit_imports(NonlinearSolveBase) === nothing
+        @test check_all_qualified_accesses_via_owners(NonlinearSolveBase) === nothing
+    end
+end
