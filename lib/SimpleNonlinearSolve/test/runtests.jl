a9f3bee531179362ba7e162eda6702c8cf5bd89b
Avik Pal <avikpal@mit.edu>
2024-10-07 20:27:50 -0400

    test: wrap in a global testset

--- lib/SimpleNonlinearSolve/test/runtests.jl
+++ lib/SimpleNonlinearSolve/test/runtests.jl
@@ -1,4 +1,4 @@
-using TestItemRunner, InteractiveUtils, Pkg
+using TestItemRunner, InteractiveUtils, Pkg, Test
 
 @info sprint(InteractiveUtils.versioninfo)
 
@@ -7,8 +7,10 @@ const GROUP = lowercase(get(ENV, "GROUP", "All"))
 (GROUP == "all" || GROUP == "cuda") && Pkg.add(["CUDA"])
 (GROUP == "all" || GROUP == "adjoint") && Pkg.add(["SciMLSensitivity"])
 
-if GROUP == "all"
-    @run_package_tests
-else
-    @run_package_tests filter = ti -> (Symbol(GROUP) in ti.tags)
+@testset "SimpleNonlinearSolve.jl" begin
+    if GROUP == "all"
+        @run_package_tests
+    else
+        @run_package_tests filter = ti -> (Symbol(GROUP) in ti.tags)
+    end
 end
