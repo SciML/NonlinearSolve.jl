89d76b06e5efa5f0cdfe05ff2167d4bb2368f53d
Avik Pal <avikpal@mit.edu>
2024-10-21 20:06:35 -0400

    chore: fix QA testing

--- lib/BracketingNonlinearSolve/test/qa_tests.jl
+++ lib/BracketingNonlinearSolve/test/qa_tests.jl
@@ -10,7 +10,8 @@ end
     import ForwardDiff
     using ExplicitImports, BracketingNonlinearSolve
 
-    @test check_no_implicit_imports(BracketingNonlinearSolve; skip = (Base, Core)) === nothing
+    @test check_no_implicit_imports(BracketingNonlinearSolve; skip = (Base, Core)) ===
+          nothing
     @test check_no_stale_explicit_imports(BracketingNonlinearSolve) === nothing
     @test check_all_qualified_accesses_via_owners(BracketingNonlinearSolve) === nothing
 end
