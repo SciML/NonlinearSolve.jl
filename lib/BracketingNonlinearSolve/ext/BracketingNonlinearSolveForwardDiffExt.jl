c108291ad9dbf5fc5f01afac2fefc368a89ac41b
Avik Pal <avikpal@mit.edu>
2024-10-29 15:27:08 -0400

    ci(github-actions): nonlinearsolvebase and bracketingnonlinearsolve

--- lib/BracketingNonlinearSolve/ext/BracketingNonlinearSolveForwardDiffExt.jl
+++ lib/BracketingNonlinearSolve/ext/BracketingNonlinearSolveForwardDiffExt.jl
@@ -7,19 +7,23 @@ using SciMLBase: SciMLBase, IntervalNonlinearProblem
 
 using BracketingNonlinearSolve: Bisection, Brent, Alefeld, Falsi, ITP, Ridder
 
+const DualIntervalNonlinearProblem{T, V, P} = IntervalNonlinearProblem{
+    uType, iip, <:Union{<:Dual{T, V, P}, <:AbstractArray{<:Dual{T, V, P}}}
+} where {uType, iip}
+
 for algT in (Bisection, Brent, Alefeld, Falsi, ITP, Ridder)
     @eval function CommonSolve.solve(
-            prob::IntervalNonlinearProblem{
-                uType, iip, <:Union{<:Dual{T, V, P}, <:AbstractArray{<:Dual{T, V, P}}}},
-            alg::$(algT),
-            args...;
-            kwargs...) where {uType, iip, T, V, P}
+            prob::DualIntervalNonlinearProblem{T, V, P}, alg::$(algT), args...;
+            kwargs...
+    ) where {T, V, P}
         sol, partials = nonlinearsolve_forwarddiff_solve(prob, alg, args...; kwargs...)
         dual_soln = nonlinearsolve_dual_solution(sol.u, partials, prob.p)
         return SciMLBase.build_solution(
-            prob, alg, dual_soln, sol.resid; sol.retcode, sol.stats,
-            sol.original, left = Dual{T, V, P}(sol.left, partials),
-            right = Dual{T, V, P}(sol.right, partials))
+            prob, alg, dual_soln, sol.resid;
+            sol.retcode, sol.stats, sol.original,
+            left = Dual{T, V, P}(sol.left, partials),
+            right = Dual{T, V, P}(sol.right, partials)
+        )
     end
 end
 
--- lib/BracketingNonlinearSolve/test/qa_tests.jl
+++ lib/BracketingNonlinearSolve/test/qa_tests.jl
@@ -1,7 +1,12 @@
 @testitem "Aqua" tags=[:core] begin
     using Aqua, BracketingNonlinearSolve
 
-    Aqua.test_all(BracketingNonlinearSolve; piracies = false, ambiguities = false)
+    Aqua.test_all(
+        BracketingNonlinearSolve;
+        piracies = false, ambiguities = false, stale_deps = false, deps_compat = false
+    )
+    Aqua.test_stale_deps(BracketingNonlinearSolve; ignore = [:SciMLJacobianOperators])
+    Aqua.test_deps_compat(BracketingNonlinearSolve; ignore = [:SciMLJacobianOperators])
     Aqua.test_piracies(BracketingNonlinearSolve; treat_as_own = [IntervalNonlinearProblem])
     Aqua.test_ambiguities(BracketingNonlinearSolve; recursive = false)
 end
