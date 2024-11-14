3fbb0f86797b849018f9dc3c3c0197c33d69febb
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-11-14 09:21:17 -0100

    Create SCCNonlinearSolve

---
+++ lib/SCCNonlinearSolve/src/SCCNonlinearSolve.jl
@@ -0,0 +1,39 @@
+module SCCNonlinearSolve
+
+import SciMLBase
+
+function SciMLBase.solve(prob::SciMLBase.SCCNonlinearProblem, alg; kwargs...)
+	numscc = length(prob.probs)
+	sols = [SciMLBase.build_solution(prob, nothing, prob.u0, convert(eltype(prob.u0),NaN)*prob.u0) for prob in prob.probs]
+	u = reduce(vcat,[prob.u0 for prob in prob.probs])
+	resid = copy(u)
+
+    earlyexit = false
+    lasti = 1
+	for i in 1:numscc
+		prob.explictfuns![i](prob.probs[i].p[1],sols)
+		sol = SciMLBase.solve(prob.probs[i], alg; kwargs...)
+		_sol = SciMLBase.build_solution(prob.probs[i], nothing, sol.u, sol.resid, retcode = sol.retcode)
+		sols[i] = _sol
+        
+        if !SciMLBase.successful_retcode(_sol)
+            earlyexit = true
+            lasti = i
+            break
+        end
+	end
+	
+	# TODO: fix allocations with a lazy concatenation
+	u .= reduce(vcat,sols)
+	resid .= reduce(vcat,getproperty.(sols,:resid))
+
+    if earlyexit
+        retcode = sols[lasti].retcode
+    else
+        retcode = SciMLBase.ReturnCode.Success
+    end
+
+	SciMLBase.build_solution(prob, alg, u, resid; retcode, original = sols)
+end
+
+end
---
+++ lib/SCCNonlinearSolve/test/qa_tests.jl
@@ -0,0 +1,22 @@
+@testitem "Aqua" tags=[:core] begin
+    using Aqua, SCCNonlinearSolve
+
+    Aqua.test_all(
+        SCCNonlinearSolve;
+        piracies = false, ambiguities = false, stale_deps = false, deps_compat = false
+    )
+    Aqua.test_stale_deps(SCCNonlinearSolve; ignore = [:SciMLJacobianOperators])
+    Aqua.test_deps_compat(SCCNonlinearSolve; ignore = [:SciMLJacobianOperators])
+    Aqua.test_piracies(SCCNonlinearSolve)
+    Aqua.test_ambiguities(SCCNonlinearSolve; recursive = false)
+end
+
+@testitem "Explicit Imports" tags=[:core] begin
+    using ExplicitImports, SCCNonlinearSolve
+
+    @test check_no_implicit_imports(
+        SCCNonlinearSolve; skip = (Base, Core, SciMLBase)
+    ) === nothing
+    @test check_no_stale_explicit_imports(SCCNonlinearSolve) === nothing
+    @test check_all_qualified_accesses_via_owners(SCCNonlinearSolve) === nothing
+end
