714687ea365d11c5663097517cca87674e6ccdea
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-11-15 23:18:26 -0100

    Update lib/SCCNonlinearSolve/test/qa_tests.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/SCCNonlinearSolve/test/qa_tests.jl
+++ lib/SCCNonlinearSolve/test/qa_tests.jl
@@ -5,8 +5,10 @@
         SCCNonlinearSolve;
         piracies = false, ambiguities = false, stale_deps = false, deps_compat = false
     )
-    Aqua.test_stale_deps(SCCNonlinearSolve; ignore = [:SciMLJacobianOperators, :NonlinearSolveBase])
-    Aqua.test_deps_compat(SCCNonlinearSolve; ignore = [:SciMLJacobianOperators, :NonlinearSolveBase])
+    Aqua.test_stale_deps(
+        SCCNonlinearSolve; ignore = [:SciMLJacobianOperators, :NonlinearSolveBase])
+    Aqua.test_deps_compat(
+        SCCNonlinearSolve; ignore = [:SciMLJacobianOperators, :NonlinearSolveBase])
     Aqua.test_piracies(
         SCCNonlinearSolve; treat_as_own = [SCCNonlinearSolve.SciMLBase.solve])
     Aqua.test_ambiguities(SCCNonlinearSolve; recursive = false)
