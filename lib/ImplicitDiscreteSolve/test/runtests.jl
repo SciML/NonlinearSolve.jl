a1a52b02fdc5cfbefe18e1c86cff76c74790d688
vyudu <vincent.duyuan@gmail.com>
2025-02-20 10:01:46 -0800

    basic implementation working

--- lib/ImplicitDiscreteSolve/test/runtests.jl
+++ lib/ImplicitDiscreteSolve/test/runtests.jl
@@ -6,15 +6,18 @@ using SimpleNonlinearSolve
 
 # Test implicit Euler using ImplicitDiscreteProblem
 @testset "Implicit Discrete System" begin
-    function lotkavolterra(u, p, t) 
+    function lotkavolterra(u, p, t)
         [1.5*u[1] - u[1]*u[2], -3.0*u[2] + u[1]*u[2]]
     end
 
     function f!(resid, u_next, u, p, t)
-        @. resid = u_next - 0.01*lotkavolterra(u_next, p, t)
+        lv = lotkavolterra(u_next, p, t)
+        resid[1] = u_next[1] - u[1] - 0.01*lv[1]
+        resid[2] = u_next[2] - u[2] - 0.01*lv[2]
+        nothing
     end
     u0 = [1., 1.]
-    tspan = (0., 1.)
+    tspan = (0., 0.5)
 
     idprob = ImplicitDiscreteProblem(f!, u0, tspan, []; dt = 0.01)
     idsol = solve(idprob, IDSolve(SimpleNewtonRaphson()))
@@ -22,5 +25,5 @@ using SimpleNonlinearSolve
     oprob = ODEProblem(lotkavolterra, u0, tspan)
     osol = solve(oprob, ImplicitEuler())
 
-    @test idsol[end] ≈ osol[end]
+    @test isapprox(idsol[end], osol[end], atol = 0.01)
 end
