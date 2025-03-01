91e2fdb587eb534752eeda0d1e0cd38032a940a4
vyudu <vincent.duyuan@gmail.com>
2025-03-01 17:35:16 -0500

    thin dependencies

--- lib/SimpleImplicitDiscreteSolve/src/SimpleImplicitDiscreteSolve.jl
+++ lib/SimpleImplicitDiscreteSolve/src/SimpleImplicitDiscreteSolve.jl
@@ -1,16 +1,9 @@
 module SimpleImplicitDiscreteSolve
 
-using SciMLBase: AbstractNonlinearProblem
 using SciMLBase
-using NonlinearSolveBase
 using SimpleNonlinearSolve
-using SymbolicIndexingInterface
-using LinearAlgebra
-using ADTypes
 using UnPack
 import OrdinaryDiffEqCore: OrdinaryDiffEqAlgorithm, alg_cache, OrdinaryDiffEqMutableCache, OrdinaryDiffEqConstantCache, get_fsalfirstlast, isfsal, initialize!, perform_step!, isdiscretecache, isdiscretealg, alg_order, beta2_default, beta1_default, dt_required
-import CommonSolve
-import DifferentiationInterface as DI
 
 using Reexport
 @reexport using DiffEqBase
