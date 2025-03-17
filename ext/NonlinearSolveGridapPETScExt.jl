3a211ad7e6c974400b3c91dfcdd390f7cb244b56
Avik Pal <avikpal@mit.edu>
2025-03-17 19:08:41 -0400

    feat: wrap GridapPETSc

---
+++ ext/NonlinearSolveGridapPETScExt.jl
@@ -0,0 +1,9 @@
+module NonlinearSolveGridapPETScExt
+
+using Gridap: Gridap
+using GridapPETSc: GridapPETSc
+
+using NonlinearSolveBase: NonlinearSolveBase
+using NonlinearSolve: NonlinearSolve, GridapPETScSNES
+
+end
