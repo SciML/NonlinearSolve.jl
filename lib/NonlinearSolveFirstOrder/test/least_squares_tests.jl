d741af0bfc16e2e01cc83db74e5e949273494e03
Avik Pal <avikpal@mit.edu>
2025-04-18 18:06:18 -0400

    test: needs NonlinearSolve

--- lib/NonlinearSolveFirstOrder/test/least_squares_tests.jl
+++ lib/NonlinearSolveFirstOrder/test/least_squares_tests.jl
@@ -54,24 +54,3 @@ end
         @test norm(sol.resid, 2) < 1e-6
     end
 end
-
-@testitem "Correct Best Solution: #565" setup=[CoreNLLSTesting] tags=[:core] begin
-    using StableRNGs
-
-    x = collect(0:0.1:10)
-
-    line_fct(x, p) = p[1] .+ p[2] .* x
-
-    y_line = line_fct(x, [1, 3])
-    y_line_n = line_fct(x, [1, 3]) + randn(StableRNG(0), length(x))
-
-    res(β, (x, y)) = line_fct(x, β) .- y
-
-    prob = NonlinearLeastSquaresProblem(res, [1, 3], p = (x, y_line_n))
-    sol1 = solve(prob; maxiters = 1000)
-
-    prob = NonlinearLeastSquaresProblem(res, [1, 5], p = (x, y_line_n))
-    sol2 = solve(prob; maxiters = 1000)
-
-    @test sol1.u ≈ sol2.u
-end
