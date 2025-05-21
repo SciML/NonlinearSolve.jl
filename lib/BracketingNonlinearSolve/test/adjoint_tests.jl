a29e84ecb39dab1d04af403f0f83e39128dac02a
jClugstor <jadonclugston@gmail.com>
2025-05-21 17:42:23 -0400

    add test

---
+++ lib/BracketingNonlinearSolve/test/adjoint_tests.jl
@@ -0,0 +1,18 @@
+@testitem "Simple Adjoint Test" tags=[:adjoint] begin
+    using ForwardDiff, Zygote, DiffEqBase
+
+    ff(u, p) = u^2 .- p[1]
+
+    function solve_nlprob(p)
+        prob = IntervalNonlinearProblem{false}(ff, [1.0, 2.0], p)
+        sol = solve(prob, Broyden())
+        res = sol isa AbstractArray ? sol : sol.u
+        return sum(abs2, res)
+    end
+
+    p = [3.0, 2.0]
+
+    ∂p_zygote = only(Zygote.gradient(solve_nlprob, p))
+    ∂p_forwarddiff = ForwardDiff.gradient(solve_nlprob, p)
+    @test ∂p_zygote ≈ ∂p_forwarddiff
+end
