ef321121604ce1d3cbe001bbdac246a8b46a9d5e
jClugstor <jadonclugston@gmail.com>
2025-05-21 17:20:29 -0400

    add Bracketing ChainRulesCoreExt

---
+++ lib/BracketingNonlinearSolve/ext/BracketingNonlinearSolveChainRulesCoreExt.jl
@@ -0,0 +1,31 @@
+module BracketingNonlinearSolveChainRulesCoreExt
+
+using CommonSolve: CommonSolve
+using ForwardDiff: ForwardDiff
+using DiffEqBase
+
+using BracketingNonlinearSolve: bracketingnonlinear_solve_up, is_extension_loaded
+
+function ChainRulesCore.rrule(
+        ::typeof(bracketingnonlinear_solve_up),
+        prob::IntervalNonlinearProblem,
+        sensealg, p, alg, args...; kwargs...
+)
+    # DiffEqBase is needed for problem/function constructor adjoint
+    !is_extension_loaded(Val(:DiffEqBase)) &&
+        error("Adjoint sensitivity analysis requires `DiffEqBase.jl` to be explicitly loaded.")
+    out = solve(prob)
+    u = out.u
+    f = SciMLBase.unwrapped_f(prob.f)
+    function ∇bracketingnonlinear_solve_up(Δ)
+        # Δ = dg/du
+        λ = only(ForwardDiff.derivative(u -> f(u, p), only(u)) \ Δ.u)
+        dgdp = -λ * ForwardDiff.derivative(p -> f(u, p), only(p))
+        return (NoTangent(), NoTangent(), NoTangent(),
+            dgdp, NoTangent(),
+            ntuple(_ -> NoTangent(), length(args))...)
+    end
+    return out, ∇bracketingnonlinear_solve_up
+end
+
+end
