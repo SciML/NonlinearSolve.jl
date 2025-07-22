47756f0e5d1de6c79ee6fc485c8491fb8d755b89
jClugstor <jadonclugston@gmail.com>
2025-07-22 12:53:43 -0400

    bring back compilation workload

--- lib/NonlinearSolveQuasiNewton/src/NonlinearSolveQuasiNewton.jl
+++ lib/NonlinearSolveQuasiNewton/src/NonlinearSolveQuasiNewton.jl
@@ -34,27 +34,27 @@ include("klement.jl")
 
 include("solve.jl")
 
-# @setup_workload begin
-#     nonlinear_functions = (
-#         (NonlinearFunction{false, NoSpecialize}((u, p) -> u .* u .- p), 0.1),
-#         (NonlinearFunction{false, NoSpecialize}((u, p) -> u .* u .- p), [0.1]),
-#         (NonlinearFunction{true, NoSpecialize}((du, u, p) -> du .= u .* u .- p), [0.1])
-#     )
+@setup_workload begin
+    nonlinear_functions = (
+        (NonlinearFunction{false, NoSpecialize}((u, p) -> u .* u .- p), 0.1),
+        (NonlinearFunction{false, NoSpecialize}((u, p) -> u .* u .- p), [0.1]),
+        (NonlinearFunction{true, NoSpecialize}((du, u, p) -> du .= u .* u .- p), [0.1])
+    )
 
-#     nonlinear_problems = NonlinearProblem[]
-#     for (fn, u0) in nonlinear_functions
-#         push!(nonlinear_problems, NonlinearProblem(fn, u0, 2.0))
-#     end
+    nonlinear_problems = NonlinearProblem[]
+    for (fn, u0) in nonlinear_functions
+        push!(nonlinear_problems, NonlinearProblem(fn, u0, 2.0))
+    end
 
-#     algs = [Broyden(), Klement()]
+    algs = [Broyden(), Klement()]
 
-#     @compile_workload begin
-#         @sync for prob in nonlinear_problems, alg in algs
-#             Threads.@spawn CommonSolve.solve(
-#                 prob, alg; abstol = 1e-2, verbose = NonlinearVerbosity())
-#         end
-#     end
-# end
+    @compile_workload begin
+        @sync for prob in nonlinear_problems, alg in algs
+            Threads.@spawn CommonSolve.solve(
+                prob, alg; abstol = 1e-2, verbose = NonlinearVerbosity())
+        end
+    end
+end
 
 @reexport using SciMLBase, NonlinearSolveBase
 
