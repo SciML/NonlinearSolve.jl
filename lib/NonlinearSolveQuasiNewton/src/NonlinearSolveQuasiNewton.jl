65376cc37a6dcfbe513ef394e7bd825a166db9a1
jClugstor <jadonclugston@gmail.com>
2025-07-22 12:53:43 -0400

    bring back compilation workload

--- lib/NonlinearSolveQuasiNewton/src/NonlinearSolveQuasiNewton.jl
+++ lib/NonlinearSolveQuasiNewton/src/NonlinearSolveQuasiNewton.jl
@@ -33,19 +33,19 @@ include("klement.jl")
 
 include("solve.jl")
 
-# @setup_workload begin
-#     nonlinear_functions = (
-#         (NonlinearFunction{false, NoSpecialize}((u, p) -> u .* u .- p), 0.1),
-#         (NonlinearFunction{false, NoSpecialize}((u, p) -> u .* u .- p), [0.1]),
-#         (NonlinearFunction{true, NoSpecialize}((du, u, p) -> du .= u .* u .- p), [0.1])
-#     )
+@setup_workload begin
+    nonlinear_functions = (
+        (NonlinearFunction{false, NoSpecialize}((u, p) -> u .* u .- p), 0.1),
+        (NonlinearFunction{false, NoSpecialize}((u, p) -> u .* u .- p), [0.1]),
+        (NonlinearFunction{true, NoSpecialize}((du, u, p) -> du .= u .* u .- p), [0.1])
+    )
 
-#     nonlinear_problems = NonlinearProblem[]
-#     for (fn, u0) in nonlinear_functions
-#         push!(nonlinear_problems, NonlinearProblem(fn, u0, 2.0))
-#     end
+    nonlinear_problems = NonlinearProblem[]
+    for (fn, u0) in nonlinear_functions
+        push!(nonlinear_problems, NonlinearProblem(fn, u0, 2.0))
+    end
 
-#     algs = [Broyden(), Klement()]
+    algs = [Broyden(), Klement()]
 
     @compile_workload begin
         @sync for prob in nonlinear_problems, alg in algs
