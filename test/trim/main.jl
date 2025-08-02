881f0581267b234cd9fddcb0cc8de9fc27727819
Romeo Valentin <romeov@stanford.edu>
2025-08-01 18:52:07 -0700

    Put trimmable code into package
    See TrimTest.jl comment for rationale.

--- test/trim/main.jl
+++ test/trim/main.jl
@@ -1,11 +1,7 @@
 using TrimTest
 
 function (@main)(argv::Vector{String})::Cint
-    λ = try
-        parse(Float64, argv[2])
-    catch
-        parse(Float64, argv[1])
-    end
+    λ = parse(Float64, argv[2])
     sol = TrimTest.minimize(λ)
     println(Core.stdout, sum(sol.u))
     return 0
