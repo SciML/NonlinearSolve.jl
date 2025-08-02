797d16d73a452b49b85cc96560077a8fb7b4f2f3
Romeo Valentin <romeov@stanford.edu>
2025-08-01 19:21:11 -0700

    Add test that `optimization_clean` doesn't trim, marked `broken`

--- test/trim/src/TrimTest.jl
+++ test/trim/src/TrimTest.jl
@@ -2,7 +2,7 @@ module TrimTest
 #=
 Currently, trimming only works if the target code is in a package. I.e., trying to trim
 ```julia
-include("trimmable_optimization.jl")
+include("optimization_trimmable.jl")
 function (@main)(argv::Vector{String})::Cint
     minimize(1.0)
     return 0
@@ -11,7 +11,7 @@ end
 or even
 ```julia
 mod MyMod
-    include("trimmable_optimization.jl")
+    include("optimization_trimmable.jl")
 end
 function (@main)(argv::Vector{String})::Cint
     MyMod.minimize(1.0)
@@ -22,5 +22,6 @@ segfaults `juliac`. Looking at the segfault stacktrace it seems the culprit is
 `const cache = init(...)`. Either way, we circumvent the segfault by putting
 this below code into a package definition.
 =#
-include("../trimmable_optimization.jl")
+include("../optimization_trimmable.jl")
+include("../optimization_clean.jl")
 end
