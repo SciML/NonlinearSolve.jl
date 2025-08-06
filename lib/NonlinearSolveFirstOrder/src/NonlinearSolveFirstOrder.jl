77699018facf3356d8d0e75f6aad0ff510a67b3d
jClugstor <jadonclugston@gmail.com>
2025-08-06 18:49:45 -0400

    get rid of DiffEqBase

--- lib/NonlinearSolveFirstOrder/src/NonlinearSolveFirstOrder.jl
+++ lib/NonlinearSolveFirstOrder/src/NonlinearSolveFirstOrder.jl
@@ -12,7 +12,7 @@ using LineSearch: BackTracking
 using StaticArraysCore: SArray
 
 using CommonSolve: CommonSolve
-using DiffEqBase: DiffEqBase    # Needed for `init` / `solve` dispatches
+#using DiffEqBase: DiffEqBase    # Needed for `init` / `solve` dispatches
 using LinearSolve: LinearSolve  # Trigger Linear Solve extension in NonlinearSolveBase
 using MaybeInplace: @bb
 using NonlinearSolveBase: NonlinearSolveBase, AbstractNonlinearSolveAlgorithm,
