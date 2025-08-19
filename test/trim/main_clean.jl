8088e497f283dae96cabf33da826ffc2c07eee61
Romeo Valentin <romeov@stanford.edu>
2025-08-19 17:24:33 +0200

    trim: `argv[2]` -> `argv[1]`
    Fixed by https://github.com/JuliaLang/julia/pull/59279

--- test/trim/main_clean.jl
+++ test/trim/main_clean.jl
@@ -1,7 +1,7 @@
 using TrimTest
 
 function (@main)(argv::Vector{String})::Cint
-    λ = parse(Float64, argv[2])
+    λ = parse(Float64, argv[1])
     sol = TrimTest.TestModuleClean.minimize(λ)
     println(Core.stdout, sum(sol.u))
     return 0
--- test/trim/main_segfault.jl
+++ test/trim/main_segfault.jl
@@ -3,7 +3,7 @@ include("./optimization_trimmable.jl")
 end
 
 function (@main)(argv::Vector{String})::Cint
-    λ = parse(Float64, argv[2])
+    λ = parse(Float64, argv[1])
     sol = MyModule.TestModuleTrimmable.minimize(λ)
     println(Core.stdout, sum(sol.u))
     return 0
