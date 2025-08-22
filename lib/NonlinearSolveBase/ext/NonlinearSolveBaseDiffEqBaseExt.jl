c1984cfb70480c85f8a4c5283f5fb97945e3e03b
jClugstor <jadonclugston@gmail.com>
2025-08-22 15:38:13 -0400

    empty diffeqbase ext

--- lib/NonlinearSolveBase/ext/NonlinearSolveBaseDiffEqBaseExt.jl
+++ lib/NonlinearSolveBase/ext/NonlinearSolveBaseDiffEqBaseExt.jl
@@ -1,16 +1,3 @@
 module NonlinearSolveBaseDiffEqBaseExt
 
-using DiffEqBase: DiffEqBase
-using SciMLBase: SciMLBase, remake
-
-using NonlinearSolveBase: NonlinearSolveBase, ImmutableNonlinearProblem
-
-function DiffEqBase.get_concrete_problem(
-        prob::ImmutableNonlinearProblem, isadapt; kwargs...)
-    u0 = SciMLBase.get_concrete_u0(prob, isadapt, nothing, kwargs)
-    u0 = SciMLBase.promote_u0(u0, prob.p, nothing)
-    p = SciMLBase.get_concrete_p(prob, kwargs)
-    return remake(prob; u0 = u0, p = p)
-end
-
 end
