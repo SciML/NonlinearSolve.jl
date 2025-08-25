398b93df5e078d65ddcabc8a819edcbfefb0eaf7
jClugstor <jadonclugston@gmail.com>
2025-08-25 15:40:36 -0400

    treat NonlinearProblems as own for piracy in aqua tests

--- lib/NonlinearSolveBase/test/runtests.jl
+++ lib/NonlinearSolveBase/test/runtests.jl
@@ -7,12 +7,14 @@ using InteractiveUtils, Test
 @testset "NonlinearSolveBase.jl" begin
     @testset "Aqua" begin
         using Aqua, NonlinearSolveBase
+        using NonlinearSolveBase: AbstractNonlinearProblem, NonlinearProblem
 
         Aqua.test_all(
             NonlinearSolveBase; piracies = false, ambiguities = false, stale_deps = false
         )
         Aqua.test_stale_deps(NonlinearSolveBase; ignore = [:TimerOutputs])
-        Aqua.test_piracies(NonlinearSolveBase)
+        #ENSEMBLE PROBLEM SHOULD BE REMOVED, THIS IS TEMPORARY FOR TESTS
+        Aqua.test_piracies(NonlinearSolveBase, treat_as_own = [AbstractNonlinearProblem, NonlinearProblem])
         Aqua.test_ambiguities(NonlinearSolveBase; recursive = false)
     end
 
