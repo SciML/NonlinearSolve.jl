92f3607d5eca30ad9352986408d1067b6728087a
jClugstor <jadonclugston@gmail.com>
2025-08-24 23:30:45 -0400

    remove stale import

--- lib/NonlinearSolveBase/src/NonlinearSolveBase.jl
+++ lib/NonlinearSolveBase/src/NonlinearSolveBase.jl
@@ -23,7 +23,7 @@ using SciMLBase: SciMLBase, ReturnCode, AbstractODEIntegrator, AbstractNonlinear
                  LinearAliasSpecifier, ImmutableNonlinearProblem, NonlinearAliasSpecifier,
                  promote_u0, get_concrete_u0, get_concrete_p,
                  has_kwargs, extract_alg, promote_u0, checkkwargs, SteadyStateProblem, EnsembleProblem
-import SciMLBase: solve, init, solve!, __init, __solve, wrap_sol, get_root_indp, isinplace, remake
+import SciMLBase: solve, init, __init, __solve, wrap_sol, get_root_indp, isinplace, remake
 using SciMLJacobianOperators: JacobianOperator, StatefulJacobianOperator
 using SciMLOperators: AbstractSciMLOperator, IdentityOperator
 using SymbolicIndexingInterface: SymbolicIndexingInterface
