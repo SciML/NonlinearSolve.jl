93b72cd86a97536683beeb8405caff7b9b95ee07
jClugstor <jadonclugston@gmail.com>
2025-08-27 10:11:33 -0400

    add extensions for autodiff

---
+++ lib/NonlinearSolveBase/ext/NonlinearSolveBaseReverseDiffExt.jl
@@ -0,0 +1,103 @@
+module NonlinearSolveBaseReverseDiffExt
+
+using NonlinearSolveBase 
+import SciMLBase: value
+import ReverseDiff
+import ArrayInterface
+
+# `ReverseDiff.TrackedArray`
+function NonlinearSolveBase.solve_up(prob::SciMLBase.AbstractDEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing}, u0::ReverseDiff.TrackedArray,
+        p::ReverseDiff.TrackedArray, args...; kwargs...)
+    ReverseDiff.track(SciMLBase.solve_up, prob, sensealg, u0, p, args...; kwargs...)
+end
+
+function NonlinearSolveBase.solve_up(prob::SciMLBase.AbstractDEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing}, u0, p::ReverseDiff.TrackedArray,
+        args...; kwargs...)
+    ReverseDiff.track(SciMLBase.solve_up, prob, sensealg, u0, p, args...; kwargs...)
+end
+
+function NonlinearSolveBase.solve_up(prob::SciMLBase.AbstractDEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing}, u0::ReverseDiff.TrackedArray, p,
+        args...; kwargs...)
+    ReverseDiff.track(SciMLBase.solve_up, prob, sensealg, u0, p, args...; kwargs...)
+end
+
+# `AbstractArray{<:ReverseDiff.TrackedReal}`
+function NonlinearSolveBase.solve_up(prob::SciMLBase.AbstractDEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing},
+        u0::AbstractArray{<:ReverseDiff.TrackedReal},
+        p::AbstractArray{<:ReverseDiff.TrackedReal}, args...;
+        kwargs...)
+    SciMLBase.solve_up(prob, sensealg, ArrayInterface.aos_to_soa(u0),
+        ArrayInterface.aos_to_soa(p), args...;
+        kwargs...)
+end
+
+function NonlinearSolveBase.solve_up(prob::SciMLBase.AbstractDEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing}, u0,
+        p::AbstractArray{<:ReverseDiff.TrackedReal},
+        args...; kwargs...)
+    SciMLBase.solve_up(
+        prob, sensealg, u0, ArrayInterface.aos_to_soa(p), args...; kwargs...)
+end
+
+function NonlinearSolveBase.solve_up(prob::SciMLBase.AbstractDEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing}, u0::ReverseDiff.TrackedArray,
+        p::AbstractArray{<:ReverseDiff.TrackedReal},
+        args...; kwargs...)
+    SciMLBase.solve_up(
+        prob, sensealg, u0, ArrayInterface.aos_to_soa(p), args...; kwargs...)
+end
+
+function NonlinearSolveBase.solve_up(prob::SciMLBase.DEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing},
+        u0::AbstractArray{<:ReverseDiff.TrackedReal}, p,
+        args...; kwargs...)
+    SciMLBase.solve_up(
+        prob, sensealg, ArrayInterface.aos_to_soa(u0), p, args...; kwargs...)
+end
+
+function NonlinearSolveBase.solve_up(prob::SciMLBase.DEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing},
+        u0::AbstractArray{<:ReverseDiff.TrackedReal}, p::ReverseDiff.TrackedArray,
+        args...; kwargs...)
+    SciMLBase.solve_up(
+        prob, sensealg, ArrayInterface.aos_to_soa(u0), p, args...; kwargs...)
+end
+
+# Required becase ReverseDiff.@grad function SciMLBase.solve_up is not supported!
+import SciMLBase: solve_up
+ReverseDiff.@grad function solve_up(prob, sensealg, u0, p, args...; kwargs...)
+    out = SciMLBase._solve_adjoint(prob, sensealg, ReverseDiff.value(u0),
+        ReverseDiff.value(p),
+        SciMLBase.ReverseDiffOriginator(), args...; kwargs...)
+    function actual_adjoint(_args...)
+        original_adjoint = out[2](_args...)
+        if isempty(args) # alg is missing
+            tuple(original_adjoint[1:4]..., original_adjoint[6:end]...)
+        else
+            original_adjoint
+        end
+    end
+    Array(out[1]), actual_adjoint
+end
+
+end
