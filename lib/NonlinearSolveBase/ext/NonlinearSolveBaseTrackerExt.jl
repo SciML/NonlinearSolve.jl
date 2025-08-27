84f83da35e1517c94de2a10dba5fde5dc8365a53
jClugstor <jadonclugston@gmail.com>
2025-08-27 10:11:33 -0400

    add extensions for autodiff

---
+++ lib/NonlinearSolveBase/ext/NonlinearSolveBaseTrackerExt.jl
@@ -0,0 +1,49 @@
+module NonlinearSolveBaseTrackerExt
+
+using NonlinearSolveBase
+import SciMLBase: value
+import Tracker
+
+function SciMLBase.solve_up(prob::SciMLBase.AbstractDEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing}, u0::Tracker.TrackedArray,
+        p::Tracker.TrackedArray, args...; kwargs...)
+    Tracker.track(SciMLBase.solve_up, prob, sensealg, u0, p, args...; kwargs...)
+end
+
+function SciMLBase.solve_up(prob::SciMLBase.AbstractDEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing}, u0::Tracker.TrackedArray, p, args...;
+        kwargs...)
+    Tracker.track(SciMLBase.solve_up, prob, sensealg, u0, p, args...; kwargs...)
+end
+
+function SciMLBase.solve_up(prob::SciMLBase.AbstractDEProblem,
+        sensealg::Union{
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm,
+            Nothing}, u0, p::Tracker.TrackedArray, args...;
+        kwargs...)
+    Tracker.track(SciMLBase.solve_up, prob, sensealg, u0, p, args...; kwargs...)
+end
+
+Tracker.@grad function SciMLBase.solve_up(prob,
+        sensealg::Union{Nothing,
+            SciMLBase.AbstractOverloadingSensitivityAlgorithm
+        },
+        u0, p, args...;
+        kwargs...)
+    sol,
+    pb_f = SciMLBase._solve_adjoint(
+        prob, sensealg, Tracker.data(u0), Tracker.data(p),
+        SciMLBase.TrackerOriginator(), args...; kwargs...)
+
+    if sol isa AbstractArray
+        !hasfield(typeof(sol), :u) && return sol, pb_f # being safe here
+        return sol.u, pb_f # AbstractNoTimeSolution isa AbstractArray
+    end
+    return convert(AbstractArray, sol), pb_f
+end
+
+end
