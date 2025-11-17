11735a012c4cf99b93aebe5333ed9dbc0cf9b430
Oscar Smith <oscardssmith@gmail.com>
2025-11-17 14:15:00 -0500

    fix Non-Numeric-u0

--- test/core_tests.jl
+++ test/core_tests.jl
@@ -444,3 +444,10 @@ end
     sol = solve(prob)
     @test SciMLBase.successful_retcode(sol)
 end
+
+@testitem "NonNumberEltype error" tags=[:core] begin
+    u0_broken = [rand(2), rand(2)]
+    f(u,p) = u
+    prob = NonlinearProblem(f, u0_broken)
+    @test_throws SciMLBase.NonNumberEltypeError solve(prob)=
+end
