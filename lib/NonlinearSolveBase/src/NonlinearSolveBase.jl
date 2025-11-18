d1126b222b711e6668aed4a394b5a45ec9dc070c
termi-official <termi-official@users.noreply.github.com>
2025-11-18 20:32:53 +0100

    Export forcing

--- lib/NonlinearSolveBase/src/NonlinearSolveBase.jl
+++ lib/NonlinearSolveBase/src/NonlinearSolveBase.jl
@@ -96,6 +96,8 @@ export RelTerminationMode, AbsTerminationMode,
 export DescentResult, SteepestDescent, NewtonDescent, DampedNewtonDescent, Dogleg,
        GeodesicAcceleration
 
+export EisenstatWalkerForcing2
+
 export NonlinearSolvePolyAlgorithm
 
 export NonlinearVerbosity
