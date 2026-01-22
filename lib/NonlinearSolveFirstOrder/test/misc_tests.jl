65ec9a40ba40084a4ea3b701c29292c7c34d3de4
JamesWrigley <james@puiterwijk.org>
2026-01-22 10:11:44 +0100

    Reset `TrustRegion.trust_region` in `reinit!()`

--- lib/NonlinearSolveFirstOrder/test/misc_tests.jl
+++ lib/NonlinearSolveFirstOrder/test/misc_tests.jl
@@ -32,3 +32,24 @@ end
     sol = solve(prob_iip_bf, NewtonRaphson())
     @test sol.retcode == ReturnCode.Success
 end
+
+@testitem "TrustRegion reinit! resets trust_region" tags = [:core] begin
+    using NonlinearSolveFirstOrder, SciMLBase
+
+    f(u, p) = u .* u .- p
+    prob = NonlinearProblem(f, [1.0, 1.0], 2.0)
+    cache = init(prob, TrustRegion())
+
+    # Get the trust region cache
+    tr_cache = cache.trustregion_cache
+    @test tr_cache.trust_region == tr_cache.initial_trust_radius
+
+    # Solve problem to modify the trust region
+    sol = solve!(cache)
+    @test SciMLBase.successful_retcode(sol)
+    @test tr_cache.trust_region != tr_cache.initial_trust_radius
+
+    # Reinitialize and check the trust region was reset
+    reinit!(cache, [1.0, 1.0]; p = 2.0)
+    @test tr_cache.trust_region == tr_cache.initial_trust_radius
+end
