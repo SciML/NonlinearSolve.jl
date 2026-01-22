fffe2b0eb358e0fa26c4644cdc9b29f6be8ad7ac
termi-official <termi-official@users.noreply.github.com>
2026-01-22 18:27:01 +0100

    Fix reinit fo forward diff cache

--- lib/NonlinearSolveBase/src/forward_diff.jl
+++ lib/NonlinearSolveBase/src/forward_diff.jl
@@ -7,6 +7,10 @@
     partials_p
 end
 
+get_u(cache::NonlinearSolveForwardDiffCache) = get_u(cache.cache)
+get_fu(cache::NonlinearSolveForwardDiffCache) = get_fu(cache.cache)
+set_fu!(cache::NonlinearSolveForwardDiffCache, fu) = set_fu!(cache.cache, fu)
+SciMLBase.set_u!(cache::NonlinearSolveForwardDiffCache, u) = SciMLBase.set_u!(cache.cache, u)
 function NonlinearSolveBase.get_abstol(cache::NonlinearSolveForwardDiffCache)
     return NonlinearSolveBase.get_abstol(cache.cache)
 end
