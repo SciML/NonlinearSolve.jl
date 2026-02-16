e4130cf9261fa90aeb29a9368adfc4cc0882e111
oscarddssmith <oscar.smith@juliahub.com>
2026-02-16 17:56:29 -0500

    fix modAB typos

--- lib/BracketingNonlinearSolve/test/rootfind_tests.jl
+++ lib/BracketingNonlinearSolve/test/rootfind_tests.jl
@@ -55,7 +55,7 @@ end
     prob = IntervalNonlinearProblem(quadratic_f, (1.0, 20.0), 2.0)
     ϵ = eps(Float64) # least possible tol for all methods
 
-    @testset for alg in (Bisection(), Falsi(), ITP(), Muller(), ModAB(), nothing)
+    @testset for alg in (Bisection(), Falsi(), ITP(), Muller(), nothing)
         @testset for abstol in [0.1, 0.01, 0.001, 0.0001, 1.0e-5, 1.0e-6]
             sol = solve(prob, alg; abstol)
             result_tol = abs(sol.u - sqrt(2))
@@ -65,8 +65,8 @@ end
         end
     end
 
-    @testset for alg in (Brent(), Ridder())
-        # Ridder and Brent converge rapidly so as we lower tolerance below 0.01, it
+    @testset for alg in (Brent(), Ridder(), ModAB())
+        # These solvers converge rapidly so as we lower tolerance below 0.01, it
         # converges with max precision to the solution
         @testset for abstol in [0.1]
             sol = solve(prob, alg; abstol)
