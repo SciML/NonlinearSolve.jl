d1a649162f3cc6b81d6c8829a8cea24bf96a164c
oscarddssmith <oscar.smith@juliahub.com>
2026-02-17 22:29:29 -0500

    fix test

--- lib/BracketingNonlinearSolve/test/rootfind_tests.jl
+++ lib/BracketingNonlinearSolve/test/rootfind_tests.jl
@@ -55,7 +55,7 @@ end
     prob = IntervalNonlinearProblem(quadratic_f, (1.0, 20.0), 2.0)
     ϵ = eps(Float64) # least possible tol for all methods
 
-    @testset for alg in (Bisection(), Falsi(), ITP(), Muller(), nothing)
+    @testset for alg in (Bisection(), Falsi(), ITP(), Muller())
         @testset for abstol in [0.1, 0.01, 0.001, 0.0001, 1.0e-5, 1.0e-6]
             sol = solve(prob, alg; abstol)
             result_tol = abs(sol.u - sqrt(2))
@@ -65,7 +65,7 @@ end
         end
     end
 
-    @testset for alg in (Brent(), Ridder(), ModAB())
+    @testset for alg in (Brent(), Ridder(), ModAB(), nothing)
         # These solvers converge rapidly so as we lower tolerance below 0.01, it
         # converges with max precision to the solution
         @testset for abstol in [0.1]
